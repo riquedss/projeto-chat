0bb2c92e30902e843d5e169294d8b4a229ed4e4f
Henrique Santana <hsantana.sousass@gmail.com>
2022-04-15 20:31:01 -0300

    att schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_29_171751) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_30_110812) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,7 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_29_171751) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "nickname"
-    t.string "password"
+    t.string "password_digest"
     t.string "birth_date"
     t.integer "kind"
     t.datetime "created_at", null: false
