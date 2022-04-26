601b0ebf1fbcb0b74623d4b1bd24f23fa4ff3cfb
Henrique Santana <hsantana.sousass@gmail.com>
2022-04-26 15:09:51 -0300

    modificacao na lógica de autenticacao

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,20 +2,18 @@ class AuthController < ApplicationController
   before_action :params_user, only: [:signup]
   
   def login
-    #nickname
     @user = User.find_by!(nickname: login_params[:nickname])
 
-    if @user.authenticate(login_params[:password])
+    if @user && @user.authenticate(login_params[:password])
       token = JsonWebToken::Base.encode(user_id: @user.id)
       
       render json: {token: token}, status: :ok
     else
-      render json: { message: "Password Incorrect"}, status: 401
+      render json: { message: "Password or Nickname Incorrect"}, status: 401
     end
   end
 
   def signup
-    @params_user[:kind] = 0
     @user = User.new(@params_user)
     if @user.save
       render json: @user, status: :created
@@ -26,7 +24,7 @@ class AuthController < ApplicationController
 
 private
   def params_user
-    @params_user = params.require(:user).permit(:name, :nickname, :password_digest, :birth_date)
+    @params_user = params.require(:user).permit(:name, :nickname, :password, :password_confirmation, :birth_date)
   end
 
   def login_params
